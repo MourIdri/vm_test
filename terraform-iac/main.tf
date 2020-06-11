6db34b52fbac641b882ad8a296d2946b16c5f880
mou.idri@gmail.com <mou.idri@gmail.com>
2020-06-11 17:04:19 +0200

    commit with terraform files upload initial

--- terraform-iac/main.tf
+++ terraform-iac/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_key_vault" "current_key_vault" {
 # ======================================================================================
 resource "azurerm_virtual_network" "vnet" {
   name                = "test_vnet"
-  address_space       = "${var.current_vnet_address_space}"
+  address_space       = ["${var.current_vnet_address_space}"]
   location            = "${azurerm_resource_group.current_ressources_group.location}"
   resource_group_name = "${azurerm_resource_group.current_ressources_group.name}"
 }
